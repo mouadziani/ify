0ce79419b8aff83f7e4c04ced7b4133d7487a774
EgorTrevelyan <egorepnikov@gmail.com>
2018-03-22 01:42:45 +0300

    Add parameters to CORS filter

--- backend/src/main/java/com/erepnikov/config/WebConfigurer.java
+++ backend/src/main/java/com/erepnikov/config/WebConfigurer.java
@@ -29,6 +29,16 @@ public class WebConfigurer implements EmbeddedServletContainerCustomizer {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList(
+                "X-Total-Count",
+                "Origin",
+                "X-Requested-With",
+                "Content-Type",
+                "Accept",
+                "Accept-Encoding",
+                "Accept-Language"
+        ));
+        config.setExposedHeaders(Arrays.asList("X-Total-Count"));
         if (config.getAllowedOrigins() != null && !config.getAllowedOrigins().isEmpty()) {
             source.registerCorsConfiguration("/api/**", config);
             source.registerCorsConfiguration("/management/**", config);
